9f3e8bb19cc8ab29d3d07aa538a51e6c16feeb4b
Stjepan Zugcic <stzugcic@vsite.hr>
2016-10-13 21:04:36 +0200

    Napisan Main2

--- Main/Program.cs
+++ Main/Program.cs
@@ -4,12 +4,12 @@ namespace CSharp.TipoviOperatori
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)    //unutar klase program smije biti samo jedna static main() metoda
         {
             Console.WriteLine("Krećem iz Program.Main(string[] args)");
         }
 
-        static void Main()
+        static void Main2()
         {
             Console.WriteLine("Krećem iz Program.Main()");
         }
