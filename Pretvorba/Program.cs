6ef007a82184ce2d422505a564460d12934254f0
Stjepan Zugcic <stzugcic@vsite.hr>
2016-10-15 21:57:09 +0200

    Riješeni zadaci - Pretvorba

--- Pretvorba/Program.cs
+++ Pretvorba/Program.cs
@@ -36,23 +36,26 @@ namespace Pretvorba
         {
             Student student = new Student();
             // dodati sve moguće pozive metoda za student
-
+            string student_oib = student.DajSvojOib();
+            int student_položi_ispit = student.PoložiIspit("C#");
 
             Osoba osobaS = student;
             // dodati sve moguće pozive metoda za osobaS
+            string osobaS_oib = osobaS.DajSvojOib(); 
 
-
-            Student sos = osobaS;
+            Student sos = (Student)osobaS;
             // dodati sve moguće pozive metoda za sos
-
+            string sos_oib=sos.DajSvojOib();
+            int sos_položi_ispit=sos.PoložiIspit("C#");
 
             Osoba osobaR = new Radnik();
             // dodati sve moguće pozive metoda za osobaR
+            string osobaR_oib = osobaR.DajSvojOib();
 
-
-            Student sor = osobaR;
+            Student sor =(Student) osobaR;
             // dodati sve moguće pozive metoda za sor
-
+            string sor_oib = sor.DajSvojOib();
+            int sor_položi_ispit = sor.PoložiIspit("C#");
 
         }
     }
