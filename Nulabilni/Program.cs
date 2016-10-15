2a9ca18d0718dfb150303eeb5117c6929e8bfc87
Stjepan Zugcic <stzugcic@vsite.hr>
2016-10-15 20:56:36 +0200

    Riješeni zadaci - Nulabilni

--- Nulabilni/Program.cs
+++ Nulabilni/Program.cs
@@ -12,8 +12,21 @@ namespace CSharp.TipoviOperatori
             double? nulabilniDuplić = Math.PI;
             Console.WriteLine(nulabilniDuplić);
 
-            double običniDuplic = Math.PI;
 
+            /*Zadatak1 - Dodati u Program.Main naredbu kojom se vrijednost nulabilnog
+                         tipa double postavlja na null referencu te ispisati nakon toga
+		                 sadržaj varijable.*/
+            nulabilniDuplić = null;
+            Console.WriteLine(nulabilniDuplić);
+
+            //Zadatak2
+            double običniDuplic = Math.PI;
+            //običniDuplic = null; ->nije dozvoljeno
+
+            /*Zadatak3 - Definirati nulabilnu varijablu korištenjem generičkog tipa
+                         Nullable<T> te ponoviti postupke iz ZADATKA 1.*/
+            Nullable<double> nulabilniDuplić1 = null;
+            Console.WriteLine(nulabilniDuplić1);
 
             Console.ReadKey();
         }
