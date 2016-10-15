f23ff4a28dbfcaca16d9f50310e37e8661a14eb1
Stjepan Zugcic <stzugcic@vsite.hr>
2016-10-15 21:43:09 +0200

    Riješeni zadaci - PridruživanjeBazniIzvedeni

--- PridruzivanjeBazniIzvedeni/Program.cs
+++ PridruzivanjeBazniIzvedeni/Program.cs
@@ -22,9 +22,22 @@ namespace CSharp.TipoviOperatori
         static void Main(string[] args)
         {
             Osoba osoba = new Osoba();
-
-            
             Osoba osobaStudent = new Student();
+
+            /*Zadatak1 - Dodajte u metodu Program.Main pozive svih mogućih metoda nad
+                         instancama objekata osoba i osobaStudent.*/
+            string oib_osoba = osoba.DajOib();
+            string oib_osobaStudent = osobaStudent.DajOib();
+
+            /*Zadatak2 - Deklarirajte objekt tipa Student i pokušajte ga instancirati 
+                         pozivom konstruktora tipa Osoba. Provjerite što prevoditelj 
+		                 javlja.*/
+            //Student s = new Osoba(); 	Cannot implicitly convert type 
+            //                       'CSharp.TipoviOperatori.Osoba' to 'CSharp.TipoviOperatori.Student'. 
+            //                        An explicit conversion exists (are you missing a cast?)	
+            //                        PridruzivanjeBazniIzvedeni	
+            //                      E:\Vsite\C#\Predavanja\02-TipoviOperatori\PridruzivanjeBazniIzvedeni\Program.cs	35	Active
+
 
 
 
