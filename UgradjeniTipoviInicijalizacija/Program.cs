6bb5e99886d62aac81373a009223ffbb2e11caeb
Stjepan Zugcic <stzugcic@vsite.hr>
2016-10-15 16:50:47 +0200

    Riješeni zadaci UgradjeniTipoviIncijalizacija

--- UgradjeniTipoviInicijalizacija/Program.cs
+++ UgradjeniTipoviInicijalizacija/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
 
 namespace CSharp.TipoviOperatori
 {
@@ -9,6 +7,32 @@ namespace CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
+            //Zadatak1
+            int a=new int();
+            Console.WriteLine(a); //Ispis-> 0
+            Console.ReadKey();
+
+            //Zadatak2
+            int b = new int();//Kompajler ne dozvoljava dodjeljivanje vrijednosti u konstruktoru
+            b = 1;
+           
+            //Zadatak3 
+            //String s=new String();
+            //Prevoditelj ne dozvoljava inicijalizaciju varijeble tipa string putem defaultnog konstruktora
+
+            //Zadatak 4
+            string g;
+            String s1="Vsite";//Dodjeljivanje vrijednosti u varijablu s
+            Console.WriteLine();
+            Console.ReadKey();
+            
+            
+            
+
+
+        
+
+
         }
     }
 }
