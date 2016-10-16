0c94fc24e4bc485777aca471a40bd54c1348843f
Stjepan Zugcic <stzugcic@vsite.hr>
2016-10-16 13:58:36 +0200

    Riješeni zadaci - Oprator Is

--- OperatorIs/Program.cs
+++ OperatorIs/Program.cs
@@ -11,9 +11,12 @@ namespace CSharp.TipoviOperatori
         static void Main(string[] args)
         {
             Osoba janko = new Student();
-            
             Osoba osoba = new Osoba();
 
+            Console.WriteLine(janko is Student); //janko is Student=True
+            Console.WriteLine(janko is Osoba); //janko is Osoba=True
+            Console.WriteLine(osoba is Osoba); //osoba is Osoba= True
+            Console.WriteLine(osoba is Student);//osoba is Student=False
 
             Console.ReadKey();
         }
